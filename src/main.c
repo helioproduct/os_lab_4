a8125f3cae53e785d64750e260cd3c08eca516b7
helio <helioproduct.sync@gmail.com>
2022-11-27 21:10:29 +0300

    lab4 done

--- src/main.c
+++ src/main.c
@@ -6,55 +6,94 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
-#include "string_utils.h"
-
-const int FILENAME_LIMIT = 255;
+// linux limits
+#define FILENAME_LIMIT 255
+#define FILEPATH_LIMIT 4096
 
 int main(void)
 {
+    char path_to_file[FILEPATH_LIMIT];
+
     printf("Enter path to file: ");
-    char path_to_file[FILENAME_LIMIT];
     scanf("%s", path_to_file);
 
     int source_fd = open(path_to_file, O_RDONLY);
     if (source_fd == -1)
     {
         perror("File open error\n");
-        return 3;
+        return 1;
     }
 
-    int protection = PROT_READ | PROT_WRITE;
-    int flags = MAP_SHARED | MAP_ANONYMOUS;
-    
-    float *shared = mmap(NULL, sizeof(float), protection, flags, -1, 0);
-    *shared = 0;
-
-    int id = fork();
-    if (id == -1)
+    struct stat st;
+    if (fstat(source_fd, &st) == -1)
     {
-        perror("fork error\n");
+        perror("couldn't get status\n");
         return 2;
     }
-    if (id == 0)
+ 
+    char *mapped_file = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, source_fd, 0);
+    float *shared_memory = mmap(NULL, sizeof(float), PROT_READ | PROT_WRITE, 
+                                                     MAP_SHARED | MAP_ANONYMOUS, 
+                                                     -1, 0);
+    if (mapped_file == NULL)
     {
-        float number = 0;
+        perror("file map error\n");
+        return 3;
+    }
+    if (shared_memory == NULL)
+    {
+        perror("memory map error\n");
+        return 4;
+    }
+    *shared_memory = 0; // total sum
+
+    pid_t pid = fork();
+    if (pid == -1)
+    {
+        perror("fork error\n");
+        return 5;
+    }
+    if (pid == 0) // child
+    {
+        int i = 0;
+        char c;
+        int capacity = 1;
+        int len = 0;
         char *string;
-        while ( (string = read_string(source_fd)) != NULL )
+
+        while (i < st.st_size)
         {
-            char *token;
-            while ( (token = strsep(&string, " ")) != NULL )
+            c = mapped_file[i];
+
+            if (c == ' ' || c == '\n' || c == EOF)
             {
-                number = atof(token);
-                shared[0] += number;
+                *shared_memory += atof(string);
+                len = 0;
+                free(string);
             }
+            else
+            {
+                if (len == 0)
+                {
+                    string = malloc(sizeof(char) * capacity);
+                }
+                if (len >= capacity)
+                {
+                    capacity *= 2;
+                    string = (char*)realloc(string, capacity * sizeof(char));
+                }
+                string[len++] = c;
+            }
+            i++;
         }
     }
     else
     {
-        wait(NULL);
-        printf("%f\n", shared[0]);
-        munmap(shared, sizeof(float));
+        waitpid(pid, NULL, 0);
+        printf("%f\n", *shared_memory);
+        munmap(shared_memory, sizeof(float));
     }
     return 0;
 }
